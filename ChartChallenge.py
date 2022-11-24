260250f808f4ed7c0a1d750a6b4757bfc29c5662
Bagonzo <halari.shanpru@gmail.com>
2022-11-25 02:00:18 +0530

    Chart Challenge

---
+++ ChartChallenge.py
@@ -0,0 +1,8 @@
+# Give a JSON data some_var = { "x": 100, "y": 120, "z": 50 }. 
+# Write a code to visualize the data in a chart?
+# Note:
+# Include the language and any library used as a comment.
+# You can use chart of your choice.
+# If you wish you can give code for multiple charts.
+# You can use any programming language of your choice.
+# Good looking output and code will be judged favorably.
